33ab883879af7db841aa4f5a80121f86dcb2c73e
AH-sunny <sunnycse03@gmail.com>
2024-02-20 22:22:56 +0600

    append part

--- function.js
+++ function.js
@@ -11,3 +11,45 @@ function setNumberByID(eventID,eventValue){
     const selectSeatElement = document.getElementById(eventID);
     selectSeatElement.innerText = eventValue;
 }
+
+//apend
+function appendSelectedSeatWithPrice(event){
+  //location
+  const apendLocation = document.getElementById('append_seat')
+
+  const parentDiv = document.createElement('div');
+  parentDiv.classList = `flex justify-between`;
+  apendLocation.appendChild(parentDiv)
+
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  p1.innerText = `${event}`;
+  p2.innerText = `Economy`;
+  p3.innerText = `550`;
+  parentDiv.appendChild(p1)
+  parentDiv.appendChild(p2)
+  parentDiv.appendChild(p3)
+
+}
+
+
+
+
+
+    /*
+    //note
+// 
+    
+//     const p = document.createElement('div');
+//     p.innerHTML =`<div class="flex justify-between"> <p id="single_selected" ></p> <p>Economy</p> <p>550</p> `;
+//     //apendLocation.appendChild(p);
+const classNumber = 0;
+
+    //location
+  
+    const apendLocation = document.querySelectorAll('.ape_seat')
+    console.log(apendLocation);
+    console.log(apendLocation[classNumber]);
+
+    */
--- mainFunction.js
+++ mainFunction.js
@@ -10,9 +10,6 @@ for (let i = 0; i < seats.length; i++) {
     event.target.style.backgroundColor = '#1DD100';
     event.target.style.color = 'white';
 
-    const sani = event.target
-    console.log('target : ',sani);
-
     //selected seat func
     const selectedSeat = getNumberByID('selected_seat')
     const currentSelectedSeat = selectedSeat + 1;
@@ -23,24 +20,15 @@ for (let i = 0; i < seats.length; i++) {
     const currentAvialableSeat = avilSeatNUmber - 1;
     setNumberByID('available_seat',currentAvialableSeat);
 
-
-    //location
-    const apendLocation = document.getElementById('append_seat')
-
-    //apend
-    const br = document.createElement('br')
-    //apendLocation.appendChild(br);
-
-    const p = document.createElement('div');
-    p.innerHTML ='<div class="flex justify-between"> <p id="single_selected" ></p> <p>Economy</p> <p>550</p>  ';
-    apendLocation.appendChild(p);
-
-    // clicked seat
+    //capture clicked value
     const targetValue = event.target.innerText ;
-    
-    setNumberByID('single_selected',targetValue);
+
+    //apend by function call
+    appendSelectedSeatWithPrice(targetValue);
     
 
+
+  
 
   });
 }
